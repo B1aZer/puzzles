104459091b6b9b16ac6de63d458cb2800cc2783f
Dmitry Branitskiy <dmitry.branitskiy@gmail.com>
2019-09-05 18:19:18 -0400

    Removing comments

--- daily-challenges/reverse-a-string.js
+++ daily-challenges/reverse-a-string.js
@@ -1,7 +1,6 @@
 let a = 'home sweet home!';
 function reverse(a) {
 	let newS = '';
-  //why??
   let sLen = a.length - 1;
   while (sLen >= 0) {
   	let letter = a[sLen];
@@ -19,7 +18,6 @@ function reverse(a) {
     	newS += a[sLen--];
     }
 	}
-
 	return newS;
 }
 console.assert(reverse(a) === 'home! sweet home', reverse(a));
