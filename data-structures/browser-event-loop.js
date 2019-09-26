57c5842d4de6c359fba725b6471145daaec4f4a5
Dmitry Branitskiy <dmitry.branitskiy@gmail.com>
2019-09-26 18:19:18 -0400

    Adding browser loop structure

---
+++ data-structures/browser-event-loop.js
@@ -0,0 +1,45 @@
+let run = true;
+let queue = [];
+cycle(0);
+
+function cycle(i) {
+	if (!run) {
+  	return;
+  }
+  let f = queue.shift();
+  f && f();
+	setTimeout(() => {
+  	cycle(i+1);
+  }, 1000);
+}
+
+function subProgram1() {
+	queue.push(() => {
+  	// do some code
+    alert(1);
+  })
+
+}
+
+function subProgram2() {
+	queue.push(() => {
+  	// do some code
+    alert(2);
+  })
+}
+
+function subProgram3() {
+	queue.push(() => {
+  	// do some code
+    throw "test";
+  })
+}
+
+function returnF() {
+	run = false;
+}
+
+window.onerror = function(msg, url, line) {
+ alert("ERROR: " + msg + "\n" + url + ":" + line);
+ return true;
+}
