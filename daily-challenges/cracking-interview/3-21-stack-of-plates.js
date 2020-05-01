091fc7cc114cdc3407ec386fe86d88b3cf24004e
Dmitry Branitskiy <dmitry.branitskiy@gmail.com>
2020-05-01 19:20:48 -0400

    Updating stack of plates

---
+++ daily-challenges/cracking-interview/3-21-stack-of-plates.js
@@ -0,0 +1,27 @@
+class SetOfStacks {
+	constructor() {
+  	this.maxLen = 2;
+    this.stacks = [];
+    this.len = 0;
+  }
+  getStack(i) {
+  	if (i >= 0) {
+    	return this.stacks[i];
+    }
+    return this.stacks[Math.floor(this.len / this.maxLen)];
+  }
+	push(v) {
+  	let s = this.getStack();
+    if (!s) {
+    	this.stacks.push([]);
+      s = this.stacks[this.stacks.length - 1];
+    }
+    this.len += 1;
+    return s.push(v);
+  }
+  pop(iOfSubstack) {
+  	this.len -= 1;
+  	let s = this.getStack(iOfSubstack);
+    return s.pop();
+  }
+}
