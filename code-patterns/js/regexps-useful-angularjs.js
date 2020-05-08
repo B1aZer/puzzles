20493db9a3f64a779ae016a2d51f155b5df74bf8
Dmitry Branitskiy <dmitry.branitskiy@gmail.com>
2020-05-08 11:54:54 -0400

    Updating reg exps

--- code-patterns/js/regexps-useful-angularjs.js
+++ code-patterns/js/regexps-useful-angularjs.js
@@ -7,3 +7,38 @@ var DATETIMELOCAL_REGEXP = /^(\d{4})-(\d\d)-(\d\d)T(\d\d):(\d\d)(?::(\d\d)(\.\d{
 var WEEK_REGEXP = /^(\d{4})-W(\d\d)$/;
 var MONTH_REGEXP = /^(\d{4})-(\d\d)$/;
 var TIME_REGEXP = /^(\d\d):(\d\d)(?::(\d\d)(\.\d{1,3})?)?$/;
+
+// match everything
+/[\s\S]/.test('\n')
+
+// match with backreference
+/^(a+)-\1$/.test('a-a')
+
+// match tag
+var tagName = /<([^>]+)>[^<]*<\/\1>/;
+
+// greedy and reluctant
+'<a> <strong>'.match(/^<(.*)>/)[1]  // greedy
+// a> <strong
+'<a> <strong>'.match(/^<(.?)>/)[1]  // reluctant
+// a
+
+// reluctant tag
+var tagName = /<(.+?)>.*?<\/\1>/
+
+// word boundary
+/\bell\b/.test('ell')
+
+/*
+The literal is compiled at load time. The following code will cause an
+exception when it is evaluated:
+*/
+
+function foo() {
+    /[/;
+}
+
+// replacement with backstring
+
+'axb cxd'.replace(/x/g, "[$`,$&,$']")
+// 'a[a,x,b cxd]b c[axb c,x,d]d'
